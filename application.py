4d859224a35a74f27db1aaa0debf66eb36df8c29
Harold J. Giddings <harold@giddings.me>
2017-05-23 18:01:27 -0400

    Added functionality to download an image of current radio conditions and upload it to the current Discord chatroom.

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import discord
-import asyncio
+from urllib.request import urlopen
+from discord import channel
 from plugins import utc
 from plugins import callsign
 
@@ -11,12 +12,21 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(game=discord.Game(name=' |!help'))
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!help'):
         await client.send_message(message.channel, "Just a basic bot to answer simple radio queries")  # line used to echo input not starting with [
+    if message.content.startswith('!conditions'):
+        img = urlopen("http://www.hamqsl.com/solar100sc.php").read()
+        hand0 = open("conditions.jpg", "wb")
+        hand0.write(img)
+        hand0.close()
+        await client.send_file(message.channel, 'conditions.jpg')
+
+
     if message.content.startswith('!utc'):
         await client.send_message(message.channel, utc.get_time())
     if message.content.startswith('!call'):
